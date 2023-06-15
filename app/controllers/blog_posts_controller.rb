0d7935b7b72f8e7303b6299ae16b943eac401f87
Bernat Frangi <bernat.frangi@gmail.com>
2023-06-15 17:00:17 +0100

    Update and delete post views

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -22,6 +22,25 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post = BlogPost.find(params[:id])
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def blog_post_params
